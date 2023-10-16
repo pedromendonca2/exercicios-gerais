56fda416309db9a8c6e1d5ffd687b29df0d66b4f
phsmendonca <pedro.h.mendonca@edu.ufes.br>
2023-10-16 09:59:43 -0300

    comecando TAD_04 dinamico

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_04/Includes/aluno.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aluno.h"
+
+void LeAluno(tAluno *aluno){
+    
+    scanf("%s %d %d %d %d", aluno->nome, &aluno->matricula, &aluno->n1, &aluno->n2, &aluno->n3);
+}
+
+tAluno* CriaAluno(){
+    
+    tAluno *aluno = (tAluno *) malloc (sizeof(tAluno));
+
+    aluno->nome = NULL;
+    aluno->matricula = -1;
+    aluno->n1 = -1;
+    aluno->n2 = -1;
+    aluno->n3 = -1;
+
+    return aluno;
+}
+
+int CalculaMediaAluno(tAluno *aluno){
+    return (aluno->n1 + aluno->n2 + aluno->n3)/3;
+}
+
+int ComparaMatricula(tAluno *aluno1, tAluno *aluno2){
+
+    if(aluno1->matricula > aluno2->matricula){
+        return 1;
+    } else if (aluno1->matricula < aluno2->matricula){
+        return -1;
+    } else{
+        return 0;
+    }
+
+}
+
+int VerificaAprovacao(tAluno *aluno){
+
+    if(CalculaMediaAluno(aluno) >= 7){
+        return 1;
+    } else{
+        return 0;
+    }
+
+}
+
+void ImprimeAluno(tAluno *aluno){
+    printf("%s\n", aluno->nome);
+}
+
+void ApagaAluno(tAluno *aluno){
+    free(aluno);
+}
